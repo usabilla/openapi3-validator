dc351a447d26d2b8ca4ca7566742b2b39859aae1
LouisjanB <lbonthuys@surveymonkey.com>
2021-06-11 11:07:20 +0200

    Removed color output. Added Python 3 syntax stringify the url removed unused imports

--- validator.py
+++ validator.py
@@ -1,22 +1,16 @@
 import sys
 from os import path, getcwd
 
-from colors import color
 from jsonschema.exceptions import RefResolutionError
 from openapi_spec_validator import openapi_v3_spec_validator
 from openapi_spec_validator.handlers import UrlHandler
-from six.moves.urllib.parse import urlparse
-
 
 def validate(url):
+    url = str(url[0])
     counter = 0
-
     try:
-        handler = UrlHandler('http', 'https', 'file')
-
-        if not urlparse(url).scheme:
-            url = 'file://' + path.join(getcwd(), url)
-
+        handler = UrlHandler('file')
+        url = 'file://' + path.join(getcwd(), url)
         spec = handler(url)
 
         for i in openapi_v3_spec_validator.iter_errors(spec, spec_url=url):
@@ -32,9 +26,9 @@ def validate(url):
         counter += 1
         print_error(
             counter,
-            '',
-            f'Unable to resolve {e.__context__.args[0]} in {e.args[0]}',
-            ''
+            ""
+            "Unable to resolve {e.__context__.args[0]} in {e.args[0]}",
+            ""
         )
     except BaseException:
         counter += 1
@@ -42,47 +36,32 @@ def validate(url):
     finally:
         if counter > 0:
             print()
-            print(
-                color(
-                    ' [FAIL] %d errors found ' % counter,
-                    fg='white',
-                    bg='red',
-                    style='bold'
-                )
-            )
+            print(" [FAIL] %d errors found " % counter)
             return 1
         else:
-            print(
-                color(
-                    ' [PASS] No errors found ',
-                    fg='white',
-                    bg='green',
-                    style='bold'
-                )
-            )
+            print(" [PASS] No errors found ")
+            
             return 0
 
 
 def print_error(count, path, message, instance):
     print()
-    print(
-        color('Error #%d in [%s]:' % (count, path or 'unknown'), style='bold')
-    )
+    print("Error #%d in [%s]:" % (count, path or 'unknown'))
     print("    %s" % message)
     print("    %s" % instance)
 
 
 def help():
-    print('usage: ' + path.basename(__file__) + ' <spec>')
+    print("usage: " + path.basename(__file__) + " <spec>")
 
 
 def main(argv):
     if len(argv) == 0:
-        print('Invalid usage!')
+        print("Invalid usage!")
         help()
         sys.exit(2)
 
-    sys.exit(validate(argv[0]))
+    sys.exit(validate(argv))
 
 
 if __name__ == "__main__":
